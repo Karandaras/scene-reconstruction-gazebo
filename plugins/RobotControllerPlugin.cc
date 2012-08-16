453d8b8645d1db3a1c2ae3a587d850072e495406
Bastian Klingen <bastian.klingen@rwth-aachen.de>
2012-08-16 11:55:48 +0200

    changed topics for subscriber/publisher

--- plugins/RobotControllerPlugin.cc
+++ plugins/RobotControllerPlugin.cc
@@ -1,20 +1,3 @@
-/*
- * Copyright 2011 Nate Koenig & Andrew Howard
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
-*/
-
 #include "physics/physics.h"
 #include "transport/transport.h"
 #include "plugins/RobotControllerPlugin.hh"
@@ -38,8 +21,8 @@ void RobotControllerPlugin::Load(physics::ModelPtr _model,
 
   this->jointSub = this->node->Subscribe(std::string("~/SceneReconstruction/RobotController/Joint"), &RobotControllerPlugin::OnSceneJointMsg, this);
   this->changeJointSub = this->node->Subscribe(std::string("~/SceneReconstruction/RobotController/Joint/setup"), &RobotControllerPlugin::OnSceneChangeMsg, this);
-  this->srguiSub = this->node->Subscribe(std::string("~/SceneReconstruction/Request"), &RobotControllerPlugin::OnRequestMsg, this);
-  this->srguiPub = this->node->Advertise<msgs::Response>(std::string("~/SceneReconstruction/Response"));
+  this->srguiSub = this->node->Subscribe(std::string("~/SceneReconstruction/RobotController/Request"), &RobotControllerPlugin::OnRequestMsg, this);
+  this->srguiPub = this->node->Advertise<msgs::Response>(std::string("~/SceneReconstruction/RobotController/Response"));
 
   sdf::ElementPtr jointElem;
 
